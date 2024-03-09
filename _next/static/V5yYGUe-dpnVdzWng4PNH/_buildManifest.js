2b83bb388906c0a9c9cbcf9c54120ca7ad8ec76c
Hamza Khuswan <hamzakhuswan@outlook.com>
2024-03-09 16:01:53 +0100

    Updates

---
+++ _next/static/V5yYGUe-dpnVdzWng4PNH/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/0936b97c-227610b7546fb4c5.js","static/chunks/315-bb2ac8ac11e6054a.js","static/chunks/pages/index-a6525275c056b2cf.js"],"/_error":["static/chunks/pages/_error-8a9f64139a466433.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
